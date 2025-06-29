87e5ce8482ef9fa03b7615ced677970ba07110a8
Chibuike Nnorom <nnoromchibuikepromise@gmail.com>
2025-06-29 19:30:18 +0100

    navbar started

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './App.css'
+import chatbox from "./assets/icons/chatbox.svg"
+import Home from './pages/Home'
+import About from './pages/About'
+import NavBar from './layouts/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
- 
-
+  
   return (
-    <>
-      <p className='text-[60px]'>Hello world</p>
-    </>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/instant' element={<About />} />
+        </Routes>
+      </BrowserRouter>  
+    </div>
   )
 }
 
---
+++ src/components/reuseable/InsuranceCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InsuranceCard = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default InsuranceCard
