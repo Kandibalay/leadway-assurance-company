aa34ee9764a7990373ef2af9fb9479e3878fefce
Chibuike Nnorom <nnoromchibuikepromise@gmail.com>
2025-07-23 10:24:25 +0100

    emailverify

--- Client/src/pages/auth/VerifyEmail.jsx
+++ Client/src/pages/auth/VerifyEmail.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate,Link } from 'react-router-dom';
 import axios from 'axios';
 import logo from '../../assets/icons/Logo.svg';
-import { Link } from 'react-router-dom';
 
 
 const VerifyEmail = () => {
